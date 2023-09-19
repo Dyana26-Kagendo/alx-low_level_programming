af810801213e3da70d18516e027055c89931a1e2
Dyana26-Kagendo <dyanakagendo@gmail.com>
2023-09-19 13:18:47 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen - find the length of a string
+ *@s: the string to be measured
+ *
+ *return: length of the string
+ *
+ */
+
+int _strlen(char *s)
+{
+int len;
+len = 0;
+while (s[len] != '\0')
+{
+	len++;
+}
+return (len);
+}
