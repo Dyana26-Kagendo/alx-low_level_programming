a0b2820abfb414ce320e80318794689c460a2c1d
Dyana26-Kagendo <dyanakagendo@gmail.com>
2023-09-21 11:52:56 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -16,9 +16,11 @@ char *string_toupper(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-	if (str[i] >= 'a' && str[i] <= 'z')
 	{
-		str[i] = str[i] - ('a' +  'A');
+		if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] = str[i] - 32;
+	}
 	}
 	return (str);
 }
